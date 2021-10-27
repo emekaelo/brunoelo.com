7f2fed3f77c774a52a424cd88c8ce7f7344a65e2
BrunoElo <elochux@gmail.com>
2021-10-27 13:08:34 +0100

    feat: added scully and scully blog support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+  {
     path: '',
     loadChildren: () =>
       import('./portfolio/portfolio.module').then((m) => m.PortfolioModule),
