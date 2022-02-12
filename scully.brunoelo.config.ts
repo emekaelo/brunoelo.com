e7d83d6cdb91bfc578b6aafe7a6b1ee13689d0dc
BrunoElo <elochux@gmail.com>
2022-02-12 19:45:39 +0100

    feat: added prism plugin & styles for syntax highlighting

--- scully.brunoelo.config.ts
+++ scully.brunoelo.config.ts
@@ -1,4 +1,7 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+
+setPluginConfig('md', { enableSyntaxHighlighting: true });
+
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'brunoelo',
