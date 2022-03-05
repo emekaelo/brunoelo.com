6f2bf64662e05531c097c958fc747af86859403f
BrunoElo <elochux@gmail.com>
2022-03-05 15:14:37 +0100

    test: added unit test for blog post component

--- src/app/blog/blog-post/blog-post.component.spec.ts
+++ src/app/blog/blog-post/blog-post.component.spec.ts
@@ -2,17 +2,45 @@ import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ScullyContentComponent, ScullyLibModule } from '@scullyio/ng-lib';
+import {
+  ScullyContentComponent,
+  ScullyLibModule,
+  ScullyRoute,
+} from '@scullyio/ng-lib';
+import { SeoService } from 'src/app/core/services/seo.service';
 import { BlogTagsComponent } from '../blog-categories/blog-tags.component';
 
 import { BlogPostComponent } from './blog-post.component';
+import { expect } from '@jest/globals';
 
 describe('BlogPostComponent', () => {
   let component: BlogPostComponent;
   let fixture: ComponentFixture<BlogPostComponent>;
   let blogPostDe: DebugElement;
+  let seoServiceSpy: {
+    updateTitle: jest.Mock;
+    updateMetaTags: jest.Mock;
+    updateTwitterMeta: jest.Mock;
+  };
+  const stubRoute: ScullyRoute = {
+    route: '/blog/latest-post',
+    title: 'Jamstack SEO Guide: Content SEO',
+    description: 'Sample blog description',
+    image: '../../../../assets/compressed.jpg',
+    meta: {
+      title: 'Jamstack SEO Guide: Content SEO | BrunoElo Blog',
+    },
+    category: ['new'],
+    published: true,
+    sourceFile: 'latest-post.md',
+  };
 
   beforeEach(async () => {
+    seoServiceSpy = {
+      updateTitle: jest.fn(),
+      updateMetaTags: jest.fn(),
+      updateTwitterMeta: jest.fn(),
+    };
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, ScullyLibModule],
       declarations: [
@@ -20,6 +48,7 @@ describe('BlogPostComponent', () => {
         BlogTagsComponent,
         ScullyContentComponent,
       ],
+      providers: [{ provide: SeoService, useValue: seoServiceSpy }],
     }).compileComponents();
   });
 
@@ -58,5 +87,38 @@ describe('BlogPostComponent', () => {
     const articleSubtitleDe = blogPostDe.query(By.css('.article__subtitle'));
     const articleSubtitleEl = articleSubtitleDe.nativeElement;
     expect(articleSubtitleEl.textContent).toBeDefined();
+  });
+
+  fdescribe('meta tag handler', () => {
+    beforeEach(() => {
+      component.handleMetaTags(stubRoute);
+    });
+
+    it('should call updateTitle with meta.title as argument', () => {
+      expect(seoServiceSpy.updateTitle).toHaveBeenNthCalledWith(
+        1,
+        'Jamstack SEO Guide: Content SEO | BrunoElo Blog'
+      );
+    });
+
+    it('should call updateMetaTags with 4 argument', () => {
+      expect(seoServiceSpy.updateMetaTags).toHaveBeenNthCalledWith(
+        1,
+        'Jamstack SEO Guide: Content SEO | BrunoElo Blog',
+        'Sample blog description',
+        '../../../../assets/compressed.jpg',
+        ['new']
+      );
+    });
+
+    it('should call updateTwitterMeta with 3 argument', () => {
+      console.log(seoServiceSpy.updateTwitterMeta.mock.calls);
+      expect(seoServiceSpy.updateTwitterMeta).toHaveBeenNthCalledWith(
+        1,
+        'Jamstack SEO Guide: Content SEO | BrunoElo Blog',
+        'Sample blog description',
+        '../../../../assets/compressed.jpg'
+      );
+    });
   });
 });
